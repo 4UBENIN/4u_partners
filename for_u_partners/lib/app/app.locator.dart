884a064145bcb75c676f5ed4c8934d9bcafacb49
Deo-Cell <deo-gratias.patinvoh@epitech.eu>
2025-08-23 10:41:34 +0100

    fix: add somes courses userflow modif

--- for_u_partners/lib/app/app.locator.dart
+++ for_u_partners/lib/app/app.locator.dart
@@ -16,6 +16,7 @@ import '../services/driver_service.dart';
 import '../services/pressing_service.dart';
 import '../services/sharedpreferences_service.dart';
 import '../services/wallet_service.dart';
+import '../ui/views/drivers/homemain/homemain_viewmodel.dart';
 
 final locator = StackedLocator.instance;
 
@@ -36,4 +37,5 @@ Future<void> setupLocator({
   locator.registerLazySingleton(() => DriverService());
   locator.registerLazySingleton(() => PressingService());
   locator.registerLazySingleton(() => WalletService());
+  locator.registerLazySingleton(() => HomemainViewModel());
 }
