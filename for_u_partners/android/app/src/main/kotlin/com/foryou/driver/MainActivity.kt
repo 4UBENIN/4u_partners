59a2c7744d3571a7039505383ad089d0af07f314
Deo-Cell <deo-gratias.patinvoh@epitech.eu>
2025-10-21 23:42:12 +0200

    up

--- for_u_partners/android/app/src/main/kotlin/com/foryou/driver/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.foryou.driver
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // Configuration de base, pas besoin de surcharger configureFlutterEngine
-}
